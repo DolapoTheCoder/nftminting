229ac7ace185a38988e22a0509dd4642bbcfe039
basalebi <bashiru.alebiosu-daniel@publicissapient.com>
2023-01-13 11:51:57 +0000

    New Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,73 +7,24 @@ import './compCSS/Navbar.css';
 const NavBar = () => {
 
     return (
+        <h1 className='title'>NFT MINT
         <div className="navbar">
-            <div className="text">
+            <div className="nft-mint">
                 <Link to="/">NFT Mint</Link>
             </div>
 
-            <div classame="profile">
+            <div className="profile">
                 <Link to="/profile">Profile</Link>
             </div>
 
-            <div className='text04'>
+            <div className='mint'>
                 <Link to="/mint">Mint</Link>
             </div>
+            <button className='button'>
+                Connect Wallet
+            </button>
         </div>
+        </h1>
     );
 };
 export default NavBar;
-
-
-// const Navbar = () => {
-
-//     const [connected, toggledConnect] = useState(false);
-//     const location = useLocation();
-//     const [currentAddress, updateAddress] = useState('0x');
-
-//     return (
-//         <div>
-//             <nav className='navbar'>
-//                 <ul className='navbar.ul'>
-//                     <li className='navbar.li'>
-//                         <Link to="/">
-//                             <div className=''>
-//                                 Home
-//                             </div>
-//                         </Link>
-//                     </li>
-//                     <li>
-//                         <ul>
-//                             {location.pathname === "/mint" ?
-//                                 <li>
-//                                     <Link to="/mint">Mint NFT</Link>
-//                                 </li>
-//                                 :
-//                                 <li>
-//                                     <Link to="/mint">Mint NFT</Link>
-//                                 </li>
-//                             }
-//                             {location.pathname === "/profile" ?
-//                                 <li>
-//                                     <Link to="/profile">Profile</Link>
-//                                 </li>
-//                                 :
-//                                 <li>
-//                                     <Link to="/profile">Profile</Link>
-//                                 </li>
-//                             }
-//                             <li>
-//                                 <button>{connected ? "Connected" : "Connect Wallet"}</button>
-//                             </li>
-//                         </ul>
-//                     </li>
-//                 </ul>
-//             </nav>
-//             <div>
-//             {currentAddress !== "0x" ? "Connected to":"Not Connected. Please login to view NFTs"} {currentAddress !== "0x" ? (currentAddress.substring(0,15)+'...'):""}
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default Navbar
