83b29eb617a62d9cdb4b1fc2f17c2a8332a8ef69
basalebi <bashiru.alebiosu-daniel@publicissapient.com>
2023-01-17 14:21:53 +0000

    NavBar

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import Navbar from './Navbar';
 import {useSelector} from 'react-redux';
-
+import '/Users/basalebi/Documents/GitHub/nftminting/src/components/compCSS/Home.css'
+import img from '/Users/basalebi/Documents/GitHub/nftminting/src/components/Images/img.jpeg'
 
 function Home() {
 
@@ -10,9 +11,11 @@ function Home() {
   return (
     <div>
         <Navbar/>
-      <header className='App-header'>
-        Home
-      </header>
+        <h1 className='wande'>
+        I'm chilling with my Daddy Wande Coal
+        </h1>
+        <img className='coal' src={img} alt="Logo" />
+
     </div>
   );
 }
