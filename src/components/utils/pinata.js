e230d7ac9e9fe1269d727ad82ab698464bba04e0
DolapoTheCoder <65259627+DolapoTheCoder@users.noreply.github.com>
2023-01-23 20:55:37 +0000

    form

--- src/components/utils/pinata.js
+++ src/components/utils/pinata.js
@@ -7,6 +7,7 @@ const axios = require('axios');
 export const pinJSONToIPFS = async(JSONBody) => {
     const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
     //making axios POST request to Pinata ⬇️
+    //check
     return axios 
         .post(url, JSONBody, {
             headers: {
